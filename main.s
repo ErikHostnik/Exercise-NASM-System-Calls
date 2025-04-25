954fb446fc141944eba1ffdf840e312e3ac0f18f
Erik Hostnik <erik.hostnik@gmail.com>
2025-04-25 15:14:10 +0200

    NS-4 Ustvarjanje datoteke z sistemskimi ukazi

--- main.s
+++ main.s
@@ -6,6 +6,10 @@ section .data
     dir_name    db "28859_Dir", 0
     mode_dir    equ 0o755
 
+    file_name   db "28859_file.dat", 0
+    open_flags  equ 0x241
+    file_mode   equ 0o644
+
 section .text
 _start:
     ;Ustvarjanje imenika 28859_Dir
@@ -20,6 +24,14 @@ _start:
     mov ebx, dir_name
     int 0x80
 
+    ;Ustvarjanje datoteke
+    mov eax, 0x05
+    mov ebx, file_name
+    mov ecx, open_flags
+    mov edx, file_mode
+    int 0x80
+
+    ;izhod
     mov eax, 0x01
     xor ebx, ebx
     int 0x80
